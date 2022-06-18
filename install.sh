d1ebcd42a0e9940afa60f33d49347504f7b2048b
Jerzy <jpuchalski1703@gmail.com>
2022-06-18 20:07:41 +0200

    first commit

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+sudo cp ./etc /etc -r
+
+sudo bash ./pacman-install.sh
+
+git clone https://git.suckless.org/dwm
+git clone https://git.suckless.org/slstatus
+git clone https://git.suckless.org/st
+
+cp .config/dwm.conf dwm/config.h
+cp .config/slstatus.conf slstatus/config.h
+cp .config/st.conf st/config.h
+
+cd dwm && sudo make clean install && cd ..
+cd st && sudo make clean install && cd ..
+cd slstatus && sudo make clean install && cd ..
+
+cp .config/.bashrc ~
+cp .config/.bash_profile ~
+cp .config/.xinitrc ~
+
+reboot
---
+++ pacman-install.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+pacman -Syu a52dec acl adobe-source-code-pro-fonts adwaita-icon-theme alsa-lib alsa-plugins alsa-topology-conf alsa-ucm-conf \
+alsa-utils amdvlk aom archlinux-keyring argon2 at-spi2-atk at-spi2-core atk attr audit autoconf automake avahi \
+base bash binutils bison boost-libs botan box2d brotli bubblewrap bzip2 c-ares ca-certificates ca-certificates-mozilla \
+ca-certificates-utils cairo cantarell-fonts clucene cmake cmus code coreutils cryptsetup curl dav1d db dbus dbus-glib \
+dconf desktop-file-utils device-mapper dhclient diffutils discord double-conversion e2fsprogs efibootmgr efivar electron17 ell \
+enchant expat faad2 fakeroot ffmpeg ffmpeg4.4 file filesystem findutils firefox flac flex fontconfig freeglut freeimage freetype2 \
+fribidi gawk gc gcc gcc-libs gdbm gdk-pixbuf2 gettext giflib git glib-networking glib2 glibc glslang glu gmp gnu-free-fonts \
+gnupg gnutls gpgme gpm graphite grep groff grub gsettings-desktop-schemas gsm gst-plugins-base-libs gstreamer gtk-update-icon-cache gtk3 \
+guile gzip harfbuzz harfbuzz-icu hicolor-icon-theme hidapi htop hunspell hwdata hyphen iana-etc icu imath imv iproute2 iptables iputils \
+iso-codes iwd jack2 jasper json-c json-glib jsoncpp jxrlib kbd keyutils kmod krb5 l-smash lame lcms2 less lib32-alsa-lib \
+lib32-alsa-plugins lib32-amdvlk lib32-brotli lib32-bzip2 lib32-curl lib32-e2fsprogs lib32-expat lib32-gcc-libs lib32-glibc \
+lib32-icu lib32-keyutils lib32-krb5 lib32-libdrm lib32-libelf lib32-libffi lib32-libglvnd lib32-libgpg-error lib32-libidn2 \
+lib32-libldap lib32-libpciaccess lib32-libpsl lib32-libssh2 lib32-libtasn1 lib32-libunistring lib32-libunwind lib32-libx11 \
+lib32-libxau lib32-libxcb lib32-libxcrypt lib32-libxdamage lib32-libxdmcp lib32-libxext lib32-libxfixes lib32-libxml2 lib32-libxshmfence \
+lib32-libxss lib32-libxxf86vm lib32-llvm-libs lib32-lm_sensors lib32-mesa lib32-ncurses lib32-nspr lib32-nss lib32-openssl \
+lib32-p11-kit lib32-readline lib32-sqlite lib32-vulkan-icd-loader lib32-wayland lib32-xz lib32-zlib lib32-zstd libabw libarchive \
+libass libassuan libasyncns libatomic_ops libavc1394 libbluray libbpf libcap libcap-ng libcdr libcloudproviders libcolord \
+libcups flibdaemon libdatrie libdca libde265 libdiscid libdrm libdvbpsi libe-book libebml libedit libelf libepoxy libepubgen \
+libetonyek libevdev libevent libexttextcat libffi libfontenc libfreehand libgcrypt libglvnd libgpg-error libgudev libheif \
+libice ibidn libidn2 libiec61883 libinih libinput libisl libixion libjpeg-turbo libksba liblangtag libldap libmad libmanette \
+libmatroska libmfx libmnl libmodplug libmpc libmpcdec libmpeg2 libmspub libmwaw libnetfilter_conntrack libnfnetlink libnftnl \
+libnghttp2 libnl libnotify ibnsgif libnsl libnumbertext libodfgen libogg libomxil-bellagio liborcus libp11-kit libpagemaker \
+libpcap libpciaccess libpipeline libplacebo libpng libproxy libpsl libpulse libqxp libraw libraw1394 libreoffice-fresh \
+librevenge librsvg ibsamplerate libsasl libseccomp libsecret libsm libsndfile libsoup libsoup3 libsoxr libssh libssh2 \
+libstaroffice libstemmer libsysprof-capture libtar libtasn1 libthai libtheora libtiff libtirpc libtommath libtool libunistring \
+libunwind libupnp libusb libuv libva libvdpau libverto libvisio libvorbis libvpx libwacom libwebp libwpd libwpe libwpg \
+libwps libx11 libxau libxcb libxcomposite libxcrypt libxcursor libxcvt libxdamage libxdmcp libxext libxfixes libxfont2 \
+libxft libxi libxinerama libxkbcommon libxkbcommon-x11 libxkbfile libxml2 libxmu libxpm libxrandr libxrender libxshmfence \
+libxslt libxss libxt libxtst libxv libxxf86vm libzmf licenses linux linux-api-headers linux-firmware linux-firmware-whence \
+llvm-libs lm_sensors lpsolve lsb-release lsof lua52 lz4 lzo m4 mailcap make man-db man-pages md4c mesa minizip mkinitcpio \
+mkinitcpio-busybox mpfr mtdev nano ncurses neon nettle npth nspr nss opencore-amr openexr openjpeg2 openssl opus orc p11-kit pacman pacman-mirrorlist \
+pam pambase pango patch pciutils pcre pcre2 perl perl-error perl-mailtools perl-timedate pinentry pixman pkgconf poppler \
+popt procps-ng psmisc python qt5-base qt5-svg qt5-translations qt5-x11extras raptor rasqal rav1e re2 readline redland \
+rest rhash ripgrep run-parts sdl2 sed shaderc shadow shared-mime-info snappy sof-firmware speex speexdsp spirv-tools \
+sqlite srt sudo svt-av1 sysfsutils systemd systemd-libs systemd-sysvcompat taglib tar tcpdump texinfo thunderbird tpm2-tss \
+tracker3 tslib tzdata ufw usbutils util-linux util-linux-libs v4l-utils vi vid.stab vim vim-runtime vlc vmaf vulkan-icd-loader \
+wayland wayland-protocols webkit2gtk wget which woff2 wpebackend-fdo x264 x265 xarchiver xcb-proto xcb-util xcb-util-image \
+xcb-util-keysyms xcb-util-renderutil xcb-util-wm xdg-dbus-proxy xdg-utils xf86-input-libinput xkeyboard-config xmlsec \
+xorg-fonts-encodings xorg-server xorg-server-common xorg-setxkbmap xorg-xauth xorg-xinit xorg-xkbcomp xorg-xmodmap xorg-xprop \
+xorg-xrandr xorg-xrdb xorg-xset xorgproto xvidcore xz zenity zimg zlib zsh zstd zxing-cpp 
